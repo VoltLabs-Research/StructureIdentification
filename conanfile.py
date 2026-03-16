264c7adf0b22e1ae800f0ba7d05b432092581572
rodyherrera <contact@rodyherrera.com>
2026-03-16 01:24:59 -0300

    fix: unblock plugin publishing and Windows CoreToolkit builds

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,6 @@ class StructureIdentificationConan(ConanFile):
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
-    default_options = {
-        "hwloc/*:shared": True,
-    }
     requires = (
         "boost/1.88.0",
         "onetbb/2021.12.0",
